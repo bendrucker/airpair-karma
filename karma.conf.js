c4b7fc3347ebb9cb5f5dd7307a9d73563cc6ebbe
Ben Drucker <bvdrucker@gmail.com>
2014-11-11 15:56:07 -0500

    Add coverage

--- karma.conf.js
+++ karma.conf.js
@@ -30,13 +30,17 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
+      './src/**/*.js': ['coverage']
     },
 
+    coverageReporter: {
+      type : 'text'
+    },
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['progress', 'coverage'],
 
 
     // web server port
