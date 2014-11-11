7af423486478ed39257d61187259554aacf6159d
Ben Drucker <bvdrucker@gmail.com>
2014-11-11 11:53:10 -0500

    Add greeter

--- src/service.js
+++ src/service.js
@@ -1,9 +1,17 @@
 'use strict';
 
 angular.module('myApp')
-  .factory('Person', function () {
+  .factory('Person', function (visitor) {
     return function Person (name) {
       this.name = name;
+      this.greet = function () {
+        if (visitor.country === 'UK') {
+          console.log('Good day to you,', this.name + '.');
+        }
+        else {
+          console.log('Hey', this.name + '!');
+        }
+      };
     };
   });
 
