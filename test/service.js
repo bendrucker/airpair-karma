0eafd307ee29b4201c3e3944db779339f2e012a3
Ben Drucker <bvdrucker@gmail.com>
2014-11-11 12:16:16 -0500

    test greet

--- test/service.js
+++ test/service.js
@@ -2,8 +2,12 @@
 
 describe('Person', function () {
 
-  var Person;
+  var Person, visitor;
   beforeEach(module('myApp'));
+  beforeEach(module(function ($provide) {
+    visitor = {};
+    $provide.value('visitor', visitor);
+  }));
   beforeEach(inject(function (_Person_) {
     Person = _Person_;
   }));
@@ -12,6 +16,19 @@ describe('Person', function () {
 
     it('assigns a name', function () {
       expect(new Person('Ben')).to.have.property('name', 'Ben');
+    });
+
+  });
+
+  describe('#greet', function () {
+
+    it('greets UK visitors formally', function () {
+      visitor.country = 'UK';
+      expect(new Person('Nigel').greet()).to.equal('Good day to you, Nigel.');
+    });
+
+    it('greets others visitors informally', function () {
+      expect(new Person('Ben').greet()).to.equal('Hey Ben!');
     });
 
   });
